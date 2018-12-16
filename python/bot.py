9a1b62ab7a05f01b8f8ab828723afde4a45f79ab
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2018-12-15 22:12:18 -0700

    Can't make bot.py a class

--- python/bot.py
+++ python/bot.py
@@ -1,7 +1,31 @@
 import discord
 from discord.ext import commands
 import asyncio
+import os
+import json
 
-class Bot:
-    def __init__(self):
-        print("This is a test")
+dirname = os.path.dirname(__file__)
+try:
+    f = open(os.path.join(dirname, '../config/messages.json'))
+    werewolfMessages = json.loads(f.read())
+    f.close()
+    f2 = open(os.path.join(dirname, "../config/command_descriptions.json"))
+    commandDescriptions = json.loads(f2.read())
+    f2.close()
+    f3 = open(os.path.join(dirname, "../config/discord-config.json"))
+    config = json.loads(f3.read())
+    f3.close()
+except FileNotFoundError:
+    print("File not found")
+
+bot = commands.Bot(command_prefix = '!')
+
+@bot.event
+async def on_ready():
+    print("The werewolves are howling!")
+
+@bot.command()
+async def exit():
+    await bot.logout()
+     
+bot.run("NDk4Mjk5MTExOTI2MTM2ODM1.Dp1jKA.5cp7uYRdzEGOUchuP4MlruXAcw8")
