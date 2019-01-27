dce69c288ac8dfb9b067b7a618a9b9e2813721da
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2019-01-26 17:35:18 -0700

    Changed the way the file exceptions are handled. Now the exception messages are clearer.

--- python/bot.py
+++ python/bot.py
@@ -17,11 +17,17 @@ try:
     f2 = open(os.path.join(dirname, "../config/command_descriptions.json"))
     commandDescriptions = json.loads(f2.read())
     f2.close()
+except FileNotFoundError:
+    print("Please make sure the messages.json and the command_descriptions.json files are in the config folder and try again.")
+    exit()
+
+try:
     f3 = open(os.path.join(dirname, "../config/discord-config.json"))
     config = json.loads(f3.read())
     f3.close()
 except FileNotFoundError:
-    print("File not found")
+    print("The config file was not found. Please make sure the discord-config.py file is in the config folder. Please refer to the README for more information.")
+    exit()
 
 bot = commands.Bot(command_prefix = '!')
 
@@ -81,6 +87,7 @@ def findPerson(ctx, *args):
             name = args[0]
         else:
             name = " ".join(args[0])
+        return name
     else:
         print("Something went very wrong. Args is not of length 1")
         return None
