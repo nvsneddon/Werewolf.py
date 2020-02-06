80cf59e45fe1d14e7b0de6768269321cf81968e5
Nathaniel <nathaniel.sneddon@gmail.com>
2020-02-05 18:20:04 -0700

    Made a file that will run the bot at the right spot so that everyone can run it easily

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd $SCRIPTPATH
+if [[ -f "./venv/bin/activate" ]]
+then
+	source ./venv/bin/activate
+fi
+cd python/
+python3 main.py
