f4be40ab9e5e700fc6214c377beff767ff2e5a23
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2020-02-24 23:11:25 -0700

    Database stuff is working well

---
+++ python/database.py
@@ -0,0 +1,56 @@
+import pymongo
+import files
+
+database_config = files.readJsonFromConfig("../config/database_config.json")
+
+myclient = pymongo.MongoClient(
+    f"mongodb+srv://{database_config['user']}:{database_config['password']}@werewolf-game-cluster-xxi0b.mongodb.net/test?retryWrites=true&w=majority")
+
+
+def update_channels(server, channels):
+    mydb = myclient["games"]
+    mycol = mydb["channels"]
+
+    query = {"server": server}
+    if mycol.count_documents(query) == 1:
+        # doc = mycol.find_one(query)
+        mycol.update_one(query, {"$set": {"channels": channels}})
+
+        # print(doc["channels"])
+    elif mycol.count_documents(query) == 0:
+        doc = {
+            "server": server,
+            "channels": channels
+        }
+        x = mycol.insert_one(doc)
+        print(x.inserted_id)
+    else:
+        print("Oops!")
+
+
+def getChannels(server):
+    mydb = myclient["games"]
+    mycol = mydb["channels"]
+    query = {"server": server}
+    x = mycol.find_one(query)
+    return x["channels"]
+
+
+if __name__ == "__main__":
+    mydb = myclient["games"]
+    mycol = mydb["channels"]
+
+    update_channels(1400, {
+        "bot-admin": 2643264,
+        "town-square": 26,
+        "werewolves": 535
+    })
+
+    for x, y in getChannels(1400).items():
+        print(x, y)
+
+    # query = {"server": 25}
+    # x = mycol.find(query)
+    # print(mycol.count_documents(query))
+    # for i in x:
+    #     print(i)
