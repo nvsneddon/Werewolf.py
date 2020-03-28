3b5ce6c31bb943cbd7031a427f0a6a050583e556
Nathaniel <nathaniel.sneddon@gmail.com>
2020-03-28 01:22:33 -0600

    Change the villager thing a tiny bit

--- python/models/villager.py
+++ python/models/villager.py
@@ -9,7 +9,7 @@ villager_schema = mongothon.Schema({
     "discord_id": {"type": int, "required": True},
     "server": {"type": int, "required": True},
     "character": {"type": str, "required": True},
-    "werewolf": {"type": bool, "required": True},
+    "werewolf": {"type": bool, "default": False},
     "alive": {"type": bool, "default": True},
     "inlove": {"type": bool, "default": False}
 })
